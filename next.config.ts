7e0f10251edc2d31569a770296cd78224a0ad40b
kausik <kkausik11@g,ail.com>
2026-02-05 15:12:11 +0530

    update nextconfig for image handling

--- next.config.ts
+++ next.config.ts
@@ -13,3 +13,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+//access images cdn from another external domain another way
+// module.exports = {
+//   images: {
+//     domains: ['cdn.example.com', 'images.unsplash.com'],
+//   },
+// }
